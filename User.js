f5c1942159a30cebb09768d942b0e2d8c697e0f1
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2020-05-14 11:28:54 +0200

    Se crea una versión preeliminar de los esquemas de la bbdd en Users.js

--- User.js
+++ User.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 // Falta por configurar el acceso y los campos de la base de datos Mongo
 
 const userSchema = new mongoose.Schema({
-    nombre: String
+    nombre: String,
+    password: String,
+    fichajes: Array
+
+});
+
+const fichajesSchema = new mongoose.Schema({
+    fecha: [Date],
+    tipo: [String],
+    observaciones: [String],
+    hora: [Number]
 });
 
 const User = mongoose.model('User', userSchema);
+const Fichajes = mongoose.model('Fichaje', fichajesSchema);
 
 module.exports = User;
