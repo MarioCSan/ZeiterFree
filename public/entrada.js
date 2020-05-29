6bf420e49be524026d6f60db4d97b148e7739c8a
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2020-05-29 10:09:38 +0200

    Se eliminan los archivos relacionados con una version primitiva del servidor node

--- public/entrada.js
+++
@@ -1,8 +0,0 @@
-console.log('Client-side code running');
-
-const button = document.getElementById('myButton');
-button.addEventListener('click', function (e) {
-   
-    console.log(new Date())
-    console.log()
-});
